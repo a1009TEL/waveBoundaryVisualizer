6068b5bfa66b835f88fc947fb88832fc91f9e75d
andre <none>
2025-10-26 21:53:14 -0500

    [FIXED] TWaveAlpha

--- shaders/fragment_wave.glsl
+++ shaders/fragment_wave.glsl
@@ -7,11 +7,17 @@ out vec4 outColor;
 uniform sampler2D uColorMap;
 uniform int uColorMapIndex;
 
+uniform float uTWaveAlpha;
+
 void main() {
     float lerp_variable = vColor.x * 0.7;
     float red_white = mix(0.5, 1.0, lerp_variable);
     float white_blue = mix(0.5, 0.0, -lerp_variable);
-    outColor = texture(uColorMap, vec2(mix(red_white, white_blue, step(lerp_variable, 0.0)), (float(uColorMapIndex)+0.5) / 15.0));
+
+    vec4 color = texture(uColorMap, vec2(mix(red_white, white_blue, step(lerp_variable, 0.0)), (float(uColorMapIndex)+0.5) / 15.0));
+    float alpha = mix(1.0, uTWaveAlpha, step(0.0, vColor.y));
+
+    outColor = vec4(color.x, color.y, color.z, alpha);
     // outColor = vec4(gl_FragCoord.w, 0.0,0.0,0.0);
     // outColor = vec4(1.0,1.0,1.0,0.1);
 }
--- shaders/vertex_wave.glsl
+++ shaders/vertex_wave.glsl
@@ -12,7 +12,6 @@ uniform float uTime;
 
 uniform bool uTM;
 
-uniform float uTWaveAlpha;
 uniform float uAmplitudeScaling;
 uniform float uTimeZoom;
 uniform float uSpaceZoom;
@@ -40,23 +39,6 @@ float reflectedOblique(float eta1, float eta2, float incident_angle, float trans
         return (eta2*cos(incident_angle)-eta1*cos(transmited_angle))/(eta2*cos(incident_angle)+eta1*cos(transmited_angle));
     }
 }
-
-// vec2 transmitedTR(float eta1, float eta2, float incident_angle, float transmited_angle, int use_TM){
-//         float alpha_2 = space_scaling_beta2 * sqrt((eta2*eta2) / (eta1*eta1) * sin(incident_angle) * sin(incident_angle) - 1.0);
-//         float beta_2x = space_scaling_beta2 * eta2 / eta1 * sin(incident_angle);
-//
-//         float transmited_amplitude exp(-alpha_2 * aPosition.z) * 2.0 * sin(beta_2x * aPosition.x - uTime*time_scaling) * incident_amplitude;
-// }
-//
-// vec2 reflectedTR(float eta1, float eta2, float incident_angle, float transmited_angle, int use_TM){
-//     float a = sqrt((eta2*eta2)/(eta1*eta1) * sin(incident_angle) * sin(incident_angle) - 1.0);
-//     float d = eta2*eta2*cos(incident_angle)*cos(incident_angle) + a*a;
-//
-//     float reflected_amplitude = ((eta2*eta2)*cos(incident_angle)*cos(incident_angle) - a*a) / d;
-//     float reflected_phase_shift = (2.0 * eta1*eta2*a*cos(incident_angle)) / d;
-//
-//     return vec2(reflected_amplitude, reflected_phase_shift);
-// }
 
 void main() {
     float time = uTime / uTimeZoom;
@@ -115,6 +97,6 @@ void main() {
 
     float pos_y = mix(incident + reflected, transmited, aPosition.z > 0.0);
 
-    vColor = vec4((pos_y/incident_amplitude), 0.0, 0.0, 1.0);
+    vColor = vec4((pos_y/incident_amplitude), aPosition.z, 0.0, 1.0);
     gl_Position = uProjection * uView * uModel * vec4(aPosition.x, pos_y * uAmplitudeScaling, aPosition.z, 1.0);
 }
